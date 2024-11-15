c6072bdbd9fb0d5e49bfb4415ffaaad2f00926cc
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-15 20:58:22 +0530

    setup routing and dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
