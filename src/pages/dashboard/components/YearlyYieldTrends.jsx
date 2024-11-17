04c5a3ce1a50350e93b09bafdb83dd81aab9e4a7
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-17 23:56:47 +0530

    done yearly production single bar chart

--- src/pages/dashboard/components/YearlyYieldTrends.jsx
+++ src/pages/dashboard/components/YearlyYieldTrends.jsx
@@ -6,21 +6,7 @@ import { FARMS } from '../../../dummy-data/FarmData';
 
 const YearlyYieldTrends = () => {
   return (
-    <StyledCard>
-      <Stack sx={{ gap: '1rem' }}>
-        <DxSecondaryTypography variant="h6">
-          Yearly Yield Production Trends
-        </DxSecondaryTypography>
-      </Stack>
-      <Box sx={{ height: '300px' }}>
-        <SingleBarChart
-          graphData={FARMS[0].fields[0].yield}
-          xAxisDataKey="year"
-          barDataKey="production"
-          barFillColor="#8884d8"
-        />
-      </Box>
-    </StyledCard>
+  
   );
 };
 
