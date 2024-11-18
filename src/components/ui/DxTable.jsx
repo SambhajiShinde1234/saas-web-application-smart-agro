160d513fc0e5a2d4ba272d1d1f8f8822352f4068
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-19 00:06:22 +0530

    updated table component

--- src/components/ui/DxTable.jsx
+++ src/components/ui/DxTable.jsx
@@ -3,34 +3,37 @@ import {
   Table,
   TableBody,
   TableCell,
+  TableContainer,
   TableHead,
   TableRow,
 } from '@mui/material';
 
 const DxTable = ({ columns, data, tableHeadBgColor = '#E8EBF0' }) => {
   return (
-    <Table>
-      <TableHead sx={{ background: tableHeadBgColor }}>
-        <TableRow>
-          {columns.map((item, index) => (
-            <TableCell key={index} align={item.align} width={item.width}>
-              {item.columnName}
-            </TableCell>
-          ))}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data.map((row, index) => (
-          <TableRow key={index} hover>
-            {columns.map((column, index) => (
-              <TableCell key={index} align={column.align}>
-                {row[column.field]}
+    <TableContainer sx={{ overflowX: 'auto', maxWidth: '100%' }}>
+      <Table>
+        <TableHead sx={{ background: tableHeadBgColor }}>
+          <TableRow>
+            {columns.map((item, index) => (
+              <TableCell key={index} align={item.align} width={item.width}>
+                {item.columnName}
               </TableCell>
             ))}
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          {data.map((row, index) => (
+            <TableRow key={index} hover>
+              {columns.map((column, index) => (
+                <TableCell key={index} align={column.align}>
+                  {row[column.field]}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
