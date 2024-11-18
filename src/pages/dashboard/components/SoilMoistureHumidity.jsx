1f33d1cb0e718a729801325595d2fa069b51c6ce
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-18 19:09:17 +0530

    done soil, moisture and humidity

--- src/pages/dashboard/components/SoilMoistureHumidity.jsx
+++ src/pages/dashboard/components/SoilMoistureHumidity.jsx
@@ -4,7 +4,23 @@ import DxSecondaryTypography from '../../../components/ui/DxSecondaryTypography'
 
 const SoilMoistureHumidity = () => {
   return (
-   
+    <StyledCard>
+      <Stack sx={{ gap: '1rem' }}>
+        <DxSecondaryTypography variant="h6">
+          Soil, Moisture & Humidity Comparison
+        </DxSecondaryTypography>
+        <StyledChartBox>
+          {/* <DoubleBarChart
+          graphData={FARMS[0].fields[0].pesticideFertilizerUsage}
+          xAxisDataKey="month"
+          barLeftDataKey="pesticide"
+          barRightDataKey="fertilizer"
+          barLeftFillColor="#8884d8"
+          barRightFillColor="#82ca9d"
+        /> */}
+        </StyledChartBox>
+      </Stack>
+    </StyledCard>
   );
 };
 
