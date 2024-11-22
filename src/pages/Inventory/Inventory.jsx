604651005d5947960f6355c8936fccdfd0fb27dd
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-22 22:55:10 +0530

    added inventory columns fir table

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,11 +1,124 @@
 import { StyledCardStack } from '../../components/styled/styled';
 import DxPrimaryTypography from '../../components/ui/DxPrimaryTypography';
+import DxTable from '../../components/ui/DxTable';
+import { INVENTORY_DATA } from '../../dummy-data/InventoryData';
+
+const inventoryTableColumns = {
+  seeds: [
+    { columnName: 'Name', field: 'itemName', align: 'left', width: 150 },
+    {
+      columnName: 'Availble Quantity(Bags)',
+      field: 'quantityAvailable',
+      align: 'center',
+      width: 150,
+    },
+    {
+      columnName: 'Batch No.',
+      field: 'batchNumber',
+      align: 'center',
+      width: 150,
+    },
+    {
+      columnName: 'Expiry Date',
+      field: 'expiryDate',
+      align: 'center',
+      width: 150,
+    },
+    { columnName: 'Supplier', field: 'supplier', align: 'left', width: 150 },
+    {
+      columnName: 'Threshold',
+      field: 'reorderLevel',
+      align: 'center',
+      width: 150,
+    },
+  ],
+  fertilizers: [
+    { columnName: 'Name', field: 'itemName', align: 'left', width: 150 },
+    {
+      columnName: 'Availble Quantity(Bags)',
+      field: 'quantityAvailable',
+      align: 'center',
+      width: 150,
+    },
+    {
+      columnName: 'Batch No.',
+      field: 'batchNumber',
+      align: 'center',
+      width: 150,
+    },
+    {
+      columnName: 'Expiry Date',
+      field: 'expiryDate',
+      align: 'center',
+      width: 150,
+    },
+    { columnName: 'Supplier', field: 'supplier', align: 'left', width: 150 },
+    {
+      columnName: 'Threshold',
+      field: 'reorderLevel',
+      align: 'center',
+      width: 150,
+    },
+  ],
+  pesticides: [
+    { columnName: 'Name', field: 'itemName', align: 'left', width: 150 },
+    {
+      columnName: 'Availble Quantity(Bags)',
+      field: 'quantityAvailable',
+      align: 'center',
+      width: 150,
+    },
+    {
+      columnName: 'Batch No.',
+      field: 'batchNumber',
+      align: 'center',
+      width: 150,
+    },
+    {
+      columnName: 'Expiry Date',
+      field: 'expiryDate',
+      align: 'center',
+      width: 150,
+    },
+    { columnName: 'Supplier', field: 'supplier', align: 'left', width: 150 },
+    {
+      columnName: 'Threshold',
+      field: 'reorderLevel',
+      align: 'center',
+      width: 150,
+    },
+  ],
+  machineryTools: [
+    { columnName: 'Name', field: 'itemName', align: 'left', width: 150 },
+    {
+      columnName: 'Available Quantity(unit)',
+      field: 'quantityAvailable',
+      align: 'center',
+      width: 150,
+    },
+    { columnName: 'Model', field: 'model', align: 'left', width: 150 },
+    {
+      columnName: 'Purchased Date',
+      field: 'purchaseDate',
+      align: 'center',
+      width: 150,
+    },
+    { columnName: 'Supplier', field: 'supplier', align: 'left', width: 150 },
+    { columnName: 'condition', field: 'condition', align: 'left', width: 150 },
+  ],
+};
+
 const Inventory = () => {
   return (
     <StyledCardStack>
       <DxPrimaryTypography variant="h5">
         Inventory Mangement
       </DxPrimaryTypography>
+
+      <DxTable
+        columns={inventoryTableColumns['seeds']}
+        data={INVENTORY_DATA['seeds']}
+      />
     </StyledCardStack>
   );
 };
