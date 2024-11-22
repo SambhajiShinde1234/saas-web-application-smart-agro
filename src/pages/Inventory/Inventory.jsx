f5afcae6fdbfd940d91eea2a82fd1c8254014db1
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-22 21:45:20 +0530

    added inventory page

---
+++ src/pages/Inventory/Inventory.jsx
@@ -0,0 +1,11 @@
+import { StyledCardStack } from '../../components/styled/styled';
+import DxPrimaryTypography from '../../components/ui/DxPrimaryTypography';
+const Inventory = () => {
+  return (
+    <StyledCardStack>
+      <DxPrimaryTypography>Inventory Mangement</DxPrimaryTypography>
+    </StyledCardStack>
+  );
+};
+
+export default Inventory;
