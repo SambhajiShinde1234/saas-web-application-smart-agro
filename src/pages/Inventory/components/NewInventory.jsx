fd518016331f212412bcf77160d49d1db2fb32ef
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-11-23 22:52:57 +0530

    add new inventory page

---
+++ src/pages/Inventory/components/NewInventory.jsx
@@ -0,0 +1,12 @@
+import { StyledCardStack } from '../../../components/styled/styled';
+import DxPrimaryTypography from '../../../components/ui/DxPrimaryTypography';
+
+const NewInventory = () => {
+  return (
+    <StyledCardStack>
+      <DxPrimaryTypography variant="h6">New Inventory</DxPrimaryTypography>
+    </StyledCardStack>
+  );
+};
+
+export default NewInventory;
