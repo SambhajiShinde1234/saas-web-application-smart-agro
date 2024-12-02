0dd6625f3223d3187cbee6aa80d353e4aca5ba05
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-12-02 23:56:15 +0530

    completed add new work form

--- src/pages/manage-labour/components/NewWork.jsx
+++ src/pages/manage-labour/components/NewWork.jsx
@@ -13,7 +13,7 @@ import DxDatePicker from '../../../components/ui/DxDatePicker';
 import DxButton from '../../../components/ui/DxButton';
 
 const taskValidationSchema = yup.object({
-  work: yup.string().trim().required('Work title is required'),
+  work: yup.string().trim().required('Work name is required'),
   date: yup.string().required('Date is required'),
   totalWorkers: yup
     .number()
@@ -25,6 +25,13 @@ const taskValidationSchema = yup.object({
     .transform((value, originalValue) => (originalValue === '' ? null : value))
     .positive()
     .required('Total worker cost is required'),
+  totalDuration: yup
+    .number()
+    .transform((value, originalValue) => (originalValue === '' ? null : value))
+    .positive()
+    .required('Total work hours is required'),
+  output: yup.string().trim(),
+  effieciency: yup.string().trim(),
 });
 
 const NewWork = () => {
@@ -48,57 +55,92 @@ const NewWork = () => {
 
       <StyledCard>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Grid2 container spacing={2}>
-            <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
-              <DxTextField
-                label="Work Name"
-                name="work"
-                placeholder="Enter work name (e.g. harvesting)"
-                control={control}
-                errorMessage={errors.work?.message}
-              />
+          <StyledCardStack>
+            <Grid2 container spacing={2}>
+              <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
+                <DxTextField
+                  required
+                  label="Work Name"
+                  name="work"
+                  placeholder="Enter work name (e.g. harvesting)"
+                  control={control}
+                  errorMessage={errors.work?.message}
+                />
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
+                <DxDatePicker
+                  required
+                  label="Date"
+                  name="date"
+                  control={control}
+                  errorMessage={errors.date?.message}
+                />
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
+                <DxTextField
+                  required
+                  label="Total Workers"
+                  type="number"
+                  name="noOfWorkers"
+                  placeholder="Enter total workers"
+                  control={control}
+                  errorMessage={errors.totalWorkers?.message}
+                />
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
+                <DxTextField
+                  required
+                  label="Total Worker Cost ($)"
+                  type="number"
+                  name="totalCost"
+                  placeholder="Enter total worker cost"
+                  control={control}
+                  errorMessage={errors.totalCost?.message}
+                />
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
+                <DxTextField
+                  required
+                  label="Total Duration (hours)"
+                  type="number"
+                  name="totalDuration"
+                  placeholder="Enter total duration (e.g. 8 hours)"
+                  control={control}
+                  errorMessage={errors.totalDuration?.message}
+                />
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
+                <DxTextField
+                  label="Output"
+                  name="output"
+                  placeholder="Enter output (e.g. 10 tonnes)"
+                  control={control}
+                  errorMessage={errors.output?.message}
+                />
+              </Grid2>
+              <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
+                <DxTextField
+                  label="Efficiency"
+                  name="effieciency"
+                  placeholder="Enter efficiency (e.g. 10 tonnes/worker-hours)"
+                  control={control}
+                  errorMessage={errors.effieciency?.message}
+                />
+              </Grid2>
             </Grid2>
-            <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
-              <DxDatePicker
-                label="Date"
-                name="date"
-                control={control}
-                errorMessage={errors.date?.message}
-              />
-            </Grid2>
-            <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
-              <DxTextField
-                label="Total Workers"
-                type="number"
-                name="noOfWorkers"
-                placeholder="Enter total workers"
-                control={control}
-                errorMessage={errors.totalWorkers?.message}
-              />
-            </Grid2>
-            <Grid2 size={{ xs: 12, sm: 6, lg: 4 }}>
-              <DxTextField
-                label="Total Worker Cost"
-                type="number"
-                name="totalCost"
-                placeholder="Enter total worker cost"
-                control={control}
-                errorMessage={errors.totalCost?.message}
-              />
-            </Grid2>
-          </Grid2>
-          <StyledFlexCenter>
-            <DxButton
-              variant="secondary"
-              onClick={() => reset()}
-              sx={{ width: '120px' }}
-            >
-              Reset
-            </DxButton>
-            <DxButton type="sumbit" sx={{ width: '120px' }}>
-              Submit
-            </DxButton>
-          </StyledFlexCenter>
+            <StyledFlexCenter>
+              <DxButton
+                variant="secondary"
+                onClick={() => reset()}
+                sx={{ width: '120px' }}
+              >
+                Reset
+              </DxButton>
+              <DxButton type="sumbit" sx={{ width: '120px' }}>
+                Submit
+              </DxButton>
+            </StyledFlexCenter>
+          </StyledCardStack>
         </form>
       </StyledCard>
     </StyledCardStack>
