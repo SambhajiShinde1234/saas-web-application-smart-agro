a8180df8db96acf70c5345e0d17ead4b7761c8d1
SambhajiShinde1234 <mr.sam.shinde@gmail.com>
2024-12-29 13:40:20 +0530

    add welcome message in overview page

--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -1,10 +1,20 @@
+import { Stack } from '@mui/material';
 import { StyledCardStack } from '../../components/styled/styled';
 import DxPrimaryTypography from '../../components/ui/DxPrimaryTypography';
+import DxSecondaryTypography from '../../components/ui/DxSecondaryTypography';
 
 const Overview = () => {
   return (
     <StyledCardStack>
       <DxPrimaryTypography variant="h6">Overview</DxPrimaryTypography>
+      <Stack gap={0.5}>
+        <DxPrimaryTypography variant="h5" sx={{ fontFamily: 'cursive' }}>
+          Hello, Sambhaji ! 👋
+        </DxPrimaryTypography>
+        <DxSecondaryTypography variant="">
+          Welcome back to Smart Agro
+        </DxSecondaryTypography>
+      </Stack>
     </StyledCardStack>
   );
 };
